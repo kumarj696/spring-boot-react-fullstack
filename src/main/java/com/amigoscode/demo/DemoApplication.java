d262dc06ebf52ddb4a58b5a0fda0c5e69ac6e07e
uday global <udaykumarj461@gmail.com>
2021-09-30 17:33:55 +0530

    added notification feature

--- src/main/java/com/amigoscode/demo/DemoApplication.java
+++ src/main/java/com/amigoscode/demo/DemoApplication.java
@@ -8,5 +8,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        PrintMessage("Hello how ar you");
+
     }
 }
